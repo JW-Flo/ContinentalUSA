4f3b4599c4d6847831b88212519d96a49fe34b7e
Joe <joe@Joes-MBP.attlocal.net>
2025-05-30 19:09:34 -0500

    ios: add OpenAPI generator plugin

--- ios-client/Package.swift
+++ ios-client/Package.swift
@@ -4,5 +4,19 @@ let package = Package(
   name: "MCPClient",
   platforms: [.iOS(.v17)],
   products: [.library(name: "MCPClient", targets: ["MCPClient"])],
-  targets: [.target(name: "MCPClient", path: "Sources")]
+  dependencies: [
+    .package(
+      url: "https://github.com/apple/swift-openapi-generator.git",
+      branch: "main"
+    )
+  ],
+  targets: [
+    .target(
+      name: "MCPClient",
+      path: "Sources",
+      plugins: [
+        .plugin(name: "OpenAPIGeneratorPlugin", package: "swift-openapi-generator")
+      ]
+    )
+  ]
 )
